b1f55915bbece59577d18f90b4a0380ceeeb34a5
EthicalEarth <EthicalTerminal@gmail.com>
2018-09-22 20:52:26 +0300

    Improve error: print syntax and error message when device file not available.

--- src/main.cc
+++ src/main.cc
@@ -26,29 +26,34 @@
 #include <errno.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+//add libconfig support
+//#include <libconfig.h>
+//using namespace libconfig;
+
 
 #define I2C_ADDR 0x38           //Адрес устройства
 #define INVERT 1                //Необходимость инвертирования результата
+
                                 //Для PCF8574, нужно инвертировать при работе с релейным модулем.
 int readwritebuffer(int mode, std::bitset<8> buffer){
-char i2cbuffer[1];          //Выделяем буфер для чтения
-int i2cfd;                  //Создаем индификатор файла
+char i2cbuffer[1];              //Выделяем буфер для чтения
+int i2cfd;                      //Создаем индификатор файла
 
 i2cfd = open("/dev/i2c-1", O_RDWR); //Открываем файл
 if (i2cfd < 0) {                    //Проверка возможности открытия
-	printf("Error opening file: %s\n", strerror(errno));
-		//return 1;
+	printf("Error opening device file: %s\n", strerror(errno));
+    exit(0);
 }
 if (ioctl(i2cfd, I2C_SLAVE, I2C_ADDR) < 0) {  //Проверяем устройство на наличие
 	printf("ioctl error: %s\n", strerror(errno));
-		//return 1;
+    exit(0);
 }
 	//Читаем значение
 if(mode==0){
-	read(i2cfd, i2cbuffer, 1);  //Читаем состояние реле
+	read(i2cfd, i2cbuffer, 1);                                //Читаем состояние реле
 	close (i2cfd);
 	printf("DATA in buffer: 0x%02X\n", i2cbuffer[0]);         //Печатаем на экран
-return i2cbuffer[0];                      //Возвращаем значение
+return i2cbuffer[0];                                          //Возвращаем значение
 }
 if(mode==1){
 	i2cbuffer[0]=(int)(buffer.to_ulong());
@@ -63,7 +68,7 @@ if(mode==1){
 
 void set(std::bitset<8> buffer,int relay, bool status){	   //Функция установки бита (состояние реле)
 if (INVERT){	
-	buffer.flip();                     //Необходимо проинвертировать результат.
+	buffer.flip();                 //Необходимо проинвертировать результат.
 	   							   // 0хFF -  все реле отключены, 0x00 - все реле включены.
 	                               // После инвертирования получаем реальный статус реле 0x00 - отключено, 
 	                               //0xFF - все включено
@@ -71,7 +76,7 @@ if (INVERT){
 buffer.set(relay,status);
 
 if (INVERT){	
-	buffer.flip();                  //Необходимо проинвертировать результат.
+	buffer.flip();                 //Необходимо проинвертировать результат.
 	   							   // 0хFF -  все реле отключены, 0x00 - все реле включены.
 	                               // После инвертирования получаем реальный статус реле 0x00 - отключено, 
 	                               //0xFF - все включено
@@ -99,13 +104,16 @@ readwritebuffer(1,buffer);
 }
 
 
+									/****Main****/
 int main(int argc, char **argv){
 
-int pcfbuf = readwritebuffer(0,0);      //Выделяем локальную переменную под прочитанный буфер микросхемы
-std::bitset<8> buffer = pcfbuf; //Выделение слова для чипа.
-	   
-int relay=1,status=0;           //Порядковый номер реле, его статус
-	                               //Начальные данные.
+	//int pcfbuf;
+	int pcfbuf = readwritebuffer(0,0);        //Выделяем локальную переменную под прочитанный буфер микросхемы
+	
+	
+	std::bitset<8> buffer = pcfbuf;           //Выделение слова для чипа.
+	int relay=1,status=0;                     //Порядковый номер реле, его статус
+	                                          //Начальные данные.
 if(argc <= 1) {
 
 	printf("\n");
@@ -127,25 +135,25 @@ if(!strcmp(argv[1], "--set")) {
             	printf("'--set' operation requires two integer parameters.\n");
             } else {
 				if (relay<1|relay>8) {printf("Wrong value. Index of relay can be only 1-8.\n");return 0;}
-				               
-				set(buffer,relay-1,status);//запрос
+				set(buffer,relay-1,status);             //запрос
 								
             }
-        }
+    }
 };
 
 
 if(!strcmp(argv[1], "--reset")) {
-      if(argc >= 3) {
-         printf("'--reset' operation not requires parameters.\n"); 
-         } else {
-         reset(buffer); //запрос  
-         }
+	if(argc >= 3) {
+		printf("'--reset' operation not requires parameters.\n"); 
+		} else {
+		reset(buffer);                                  //запрос  
+		}
 };
 	
 if(not(!strcmp(argv[1], "--set")|!strcmp(argv[1], "--reset")|!strcmp(argv[1], "--help"))){
-    printf("Unknown parameter: '%s'. Type %s --help for help.\n", argv[1], argv[0]);
-   	};
+	printf("Unknown parameter: '%s'. Type %s --help for help.\n", argv[1], argv[0]);
+};
+
 }
 
 
