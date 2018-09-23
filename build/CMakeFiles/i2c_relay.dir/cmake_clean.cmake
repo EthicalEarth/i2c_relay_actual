d4c4e84de05f766812b227758306e14a9d6e8355
EthicalEarth <EthicalTerminal@gmail.com>
2018-09-23 20:34:18 +0300

    Migrate to Kdevelop and cmake based build system

---
+++ build/CMakeFiles/i2c_relay.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/i2c_relay.dir/main.cpp.o"
+  "i2c_relay.pdb"
+  "i2c_relay"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/i2c_relay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
